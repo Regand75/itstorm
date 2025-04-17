af750694d904ec44e7778d9dc66b5d414f5b0160
regand75 <regandess@gmail.com>
2025-04-17 22:06:10 +0300

    creating sections pupular articles and reviews

--- frontend/src/app/shared/components/service-card/service-card.component.ts
+++ frontend/src/app/shared/components/service-card/service-card.component.ts
@@ -1,4 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
+import {ArticleType} from "../../../../type/article.type";
 
 @Component({
   selector: 'service-card',
@@ -10,13 +12,37 @@ export class ServiceCardComponent implements OnInit {
   @Input() serviceCardData!: {
     image: string,
     title: string,
-    text: string,
+    description: string,
     price: string,
   };
+  // @Input() isDataBackend: boolean = false;
+  @Input() article!: ArticleType;
 
-  constructor() { }
+  constructor(private router: Router) { }
+
+  get cardData() {
+    return this.article || this.serviceCardData;
+  }
+
+  get cardImage(): string {
+    if (this.isDataBackend) {
+      return `http://localhost:3000/images/${this.article.image}`;
+    } else {
+      return `/assets/images/card/${this.serviceCardData.image}`; // путь внутри проекта
+    }
+  }
+
+  get isDataBackend(): boolean {
+    return !!this.article;
+  }
 
   ngOnInit(): void {
+  }
+
+  navigate() {
+    if (this.isDataBackend) {
+      this.router.navigate(['/blog/']);
+    }
   }
 
 }
